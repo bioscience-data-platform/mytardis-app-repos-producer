f071f98797da5dda8b2dac28839e2c1457809964
Ian Thomas <ianedwardthomas@gmail.com>
2012-11-21 16:56:19 -0800

    basic json call for user info

--- experiment.py
+++ experiment.py
@@ -1,20 +1,17 @@
 from tardis.apps.oaipmh.provider.experiment import DcExperimentProvider
-from tardis.tardis_portal.models import Author_Experiment, Experiment,\
-    ExperimentParameterSet, License, User
-from oaipmh.common import Identify, Header, Metadata
-import oaipmh.error
-from oaipmh.interfaces import IOAI
-from oaipmh.metadata import global_metadata_registry
-from oaipmh.server import Server, oai_dc_writer, NS_XSI
-from django.contrib.auth.models import User, Group
+
+from oaipmh.common import Metadata
+
+from oaipmh.server import oai_dc_writer
+
 
 class FederatedExperimentProvider(DcExperimentProvider):
-   
+
     def _get_experiment_metadata(self, experiment, metadataPrefix):
         return Metadata({
             '_writeMetadata': lambda e, m: oai_dc_writer(e, m),
             'title': [experiment.title],
             'description': [experiment.description],
-            'identifier': [str(experiment.id)],                 
+            'identifier': [str(experiment.id)],
             'creator': str(experiment.get_owners()[0].id)
         })
---
+++ views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+import json
+from django.contrib.auth.models import User
+
+
+def user(request, user_id):
+    try:
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return HttpResponse('User profile %s does not exist' % user_id)
+    obj = {}
+    obj['id'] = user.id
+    obj['first_name'] = user.first_name
+    obj['last_name'] = user.last_name
+    obj['email'] = user.email
+
+    return HttpResponse(json.dumps(obj), mimetype='application/json')
